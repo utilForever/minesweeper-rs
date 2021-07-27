3380988a6e0704bf86e840eef87d31ba79759313
Chris Ohk <utilForever@gmail.com>
2021-07-27 17:26:38 +0900

    feat(window): Add code to show simple message box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        MessageBoxA(None, "Hello", "World", MB_OK);
+    }
 }

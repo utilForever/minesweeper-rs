0afead5c20addc05f90797303a0d785946b39572
Chris Ohk <utilForever@gmail.com>
2021-07-28 13:33:48 +0900

    feat(grid): Add code to show a window and process event loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-use bindings::Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
-    unsafe {
-        MessageBoxA(None, "Hello", "World", MB_OK);
-    }
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    window.set_title("Minesweeper");
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }

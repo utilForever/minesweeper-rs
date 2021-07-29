215ff696d2b749d1622d44c3bfbec7664a9e5ac2
Chris Ohk <utilForever@gmail.com>
2021-07-29 13:23:08 +0900

    feat(game): Add function 'new'

--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -1,3 +1,17 @@
+use bindings::Windows::UI::Composition::ContainerVisual;
+
 pub struct Minesweeper {
     mines: Vec<bool>,
+}
+
+impl Minesweeper {
+    pub fn new(
+        container_visual: &ContainerVisual,
+        width: u32,
+        height: u32,
+    ) -> windows::Result<Self> {
+        Ok(Self {
+            mines: vec![false; (width * height) as usize],
+        })
+    }
 }

6b5c595ef550433adfa1443e0e6c8d1a14da278f
Chris Ohk <utilForever@gmail.com>
2021-07-29 13:59:31 +0900

    feat(game): Add variable 'num_mines'

--- src/minesweeper.rs
+++ src/minesweeper.rs
@@ -8,6 +8,7 @@ pub enum MineState {
 
 pub struct Minesweeper {
     mines: Vec<bool>,
+    num_mines: i32,
     mine_states: Vec<MineState>,
 }
 
@@ -15,11 +16,13 @@ impl Minesweeper {
     pub fn new(
         width: u32,
         height: u32,
+        num_mines: i32,
     ) -> windows::Result<Self> {
         let board_size = (width * height) as usize;
 
         Ok(Self {
             mines: vec![false; board_size],
+            num_mines: num_mines,
             mine_states: vec![MineState::Empty; board_size],
         })
     }

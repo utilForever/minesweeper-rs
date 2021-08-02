195d8bd39d9303bbce2d57e4dbc632c6b8c83d5e
Chris Ohk <utilForever@gmail.com>
2021-08-02 15:12:23 +0900

    feat(gui): Change code to calculate size and offset

--- src/grid.rs
+++ src/grid.rs
@@ -9,10 +9,15 @@ pub struct Grid {
     root: ContainerVisual,
 
     tile_size: Vector2,
+    margin: Vector2,
 }
 
 impl Grid {
-    pub fn new(compositor: &Compositor, tile_size: Vector2) -> windows::Result<Self> {
+    pub fn new(
+        compositor: &Compositor,
+        tile_size: Vector2,
+        margin: Vector2,
+    ) -> windows::Result<Self> {
         let compositor = compositor.clone();
         let root = compositor.CreateContainerVisual()?;
 
@@ -20,6 +25,7 @@ impl Grid {
             compositor: compositor,
             root: root,
             tile_size: tile_size.clone(),
+            margin: margin.clone(),
         })
     }
 
@@ -34,12 +40,20 @@ impl Grid {
         for x in 0..8 {
             for y in 0..8 {
                 let visual = self.compositor.CreateSpriteVisual()?;
-                visual.SetSize(Vector2::new(25.0, 25.0))?;
+                visual.SetSize(&self.tile_size)?;
                 visual.SetBrush(&color_brush)?;
-                visual.SetCenterPoint(Vector3::new(12.5, 12.5, 0.0))?;
+                visual.SetCenterPoint(Vector3::new(
+                    self.tile_size.X / 2.0,
+                    self.tile_size.Y / 2.0,
+                    0.0,
+                ))?;
                 visual.SetOffset(
-                    Vector3::new(1.25, 1.25, 0.0)
-                        + (Vector3::new(27.5, 27.5, 27.5) * Vector3::new(x as f32, y as f32, 0.0)),
+                    Vector3::new(self.margin.X / 2.0, self.margin.Y / 2.0, 0.0)
+                        + (Vector3::new(
+                            self.tile_size.X + self.margin.X,
+                            self.tile_size.Y + self.margin.Y,
+                            0.0,
+                        ) * Vector3::new(x as f32, y as f32, 0.0)),
                 )?;
                 children.InsertAtTop(&visual)?;
             }
